4d61c4b1106df8c4dbb2442c856844cca8c5fa29
Felipe Fuzisaki <felipefuzi@hotmail.com>
2022-03-20 14:56:58 -0300

    #9 - Search Contacts

--- public/js/app.js
+++ public/js/app.js
@@ -15,3 +15,27 @@ document.querySelectorAll('.btn-delete').forEach(function(btn) {
 
     });
 });
+
+document.getElementById('btn-clear').addEventListener('click', () => {
+    let input = document.getElementById('search'),
+    select = document.getElementById('filter_company_id');
+
+    input.value = "";
+    select.selectedIndex = 0;
+
+    window.location.href = window.location.href.split('?')[0];
+});
+
+const toggleClearButton = () => {
+    let query = location.search,
+    pattern = /[?&]search=/, // ?company_id=1&search=test
+    button = document.getElementById('btn-clear');
+
+    if(pattern.test(query)) {
+        button.style.display = 'block';
+    }else {
+        button.style.display = 'none';
+    }
+};
+
+toggleClearButton();
--- resources/views/contacts/_filter.blade.php
+++ resources/views/contacts/_filter.blade.php
@@ -14,7 +14,7 @@
                             <div class="input-group mb-3">
                             <input type="text" name="search" id="search" value="{{ request('search') }}" class="form-control" placeholder="Search..." aria-label="Search..." aria-describedby="button-addon2">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button">
+                                <button class="btn btn-outline-secondary" id="btn-clear" type="button">
                                     <i class="fa fa-refresh"></i>
                                     </button>
                                 <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
